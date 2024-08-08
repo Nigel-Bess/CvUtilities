8f33ad7efa18d273c53ba804abf0965ec22bf3a6
Jessica van de Ven <jessica.vandeven@fulfil.com>
2024-08-08 16:37:55 -0700

    more

--- Fulfil.Dispense/src/commands/pre_side_dispense/pre_side_dispense_response.cpp
+++ Fulfil.Dispense/src/commands/pre_side_dispense/pre_side_dispense_response.cpp
@@ -18,7 +18,11 @@ void PreSideDispenseResponse::encode_payload()
     result_json["Error"] = (int)this->success_code;
     result_json["Error_Description"] = this->error_description;
     // TODO: test encoding
-    result_json["Occupancy_Map"] = *this->occupancy_map;
+    std::vector<std::vector<int>> map;
+    for (int x = 0; x < this->occupancy_map->size(); x++) {
+        map.push_back(*this->occupancy_map.at(x));
+    }
+    result_json["Occupancy_Map"] = map;
 
     std::string json_string = result_json.dump();
     Logger::Instance()->Info("Encoding PreSideDispenseResponse as: {}", json_string);
@@ -33,12 +37,12 @@ void PreSideDispenseResponse::encode_payload()
 
 PreSideDispenseResponse::PreSideDispenseResponse(std::shared_ptr<std::string> request_id,
                                                  std::shared_ptr<std::string> primary_key_id,
-                                                 //std::shared_ptr<std::vector<std::shared_ptr<std::vector>>>> occupancy_map,
+                                                 std::shared_ptr<std::vector<std::shared_ptr<std::vector<int>>>> occupancy_map,
                                                  SideDispenseErrorCodes success_code,
                                                  std::string error_description) : 
                                                  request_id(request_id), 
                                                  primary_key_id(primary_key_id), 
-                                                 //occupancy_map(occupancy_map),
+                                                 occupancy_map(occupancy_map),
                                                  success_code(success_code), 
                                                  error_description(error_description){}
 
