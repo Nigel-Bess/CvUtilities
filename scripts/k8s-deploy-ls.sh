da3cda5f749588ed6890b6815e503152498323ef
Tim Courrejou <timcour@users.noreply.github.com>
2024-10-18 13:13:55 -0700

    fix(scripts): add copy file from pod utility (#135)

---
+++ scripts/k8s-deploy-ls.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+function display_help() {
+    echo "Usage: $0 -n <namespace> -d <deployment_name> <remote_path>"
+    echo
+    echo "Options:"
+    echo "  -n    Kubernetes namespace where the deployment is located (DEFAULT: fulfil-prod)"
+    echo "  -d    Name of the deployment."
+    echo "  -h    Display this help message."
+    echo
+    echo "Positional arguments:"
+    echo "  <remote_path>   Remote path on the pod for which files will be listed."
+    echo
+    echo "Example:"
+    echo "  $0 -n default -d my-deployment /path/on/pod"
+    exit 1
+}
+
+namespace="fulfil-prod"
+
+while getopts "n:d:h" opt; do
+    case ${opt} in
+        n ) namespace=$OPTARG ;;
+        d ) deployment_name=$OPTARG ;;
+        h ) display_help ;;
+        * ) display_help ;;
+    esac
+done
+
+shift $((OPTIND -1))
+
+if [[ -z "$1" ]]; then
+    echo "Error: Missing required remote directory arguments."
+    display_help
+fi
+
+remote_path=$1
+
+if [[ -z "$namespace" || -z "$deployment_name" ]]; then
+    echo "Error: Missing required arguments for namespace or deployment."
+    display_help
+fi
+
+pod_name=$(kubectl get pods -n $namespace -l app.kubernetes.io/name=$deployment_name -o jsonpath="{.items[0].metadata.name}")
+
+if [[ -z "$pod_name" ]]; then
+    echo "Error: Unable to find a pod for the deployment $deployment_name in namespace $namespace."
+    kubectl config get-contexts
+    echo "Are you using the right Kubernetes context?"
+    echo '  Change your context with `kubectl config use-context CONTEXT_NAME`'
+    exit 1
+fi
+
+
+kubectl exec -n $namespace $pod_name -- ls -l $remote_path
+
+if [ $? -ne 0 ]; then
+    echo "Error listing files."
+    exit 1
+fi
