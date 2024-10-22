f3b301f8f9340434caab1d80356bbdbbd4994c68
mstarkey-ff <40402078+mstarkey-ff@users.noreply.github.com>
2024-10-22 10:26:42 -0700

    feat(repack): Add all camera bays (#142)

--- Fulfil.AlliedVision/src/bag_release.cpp
+++ Fulfil.AlliedVision/src/bag_release.cpp
@@ -12,10 +12,17 @@ int main()
         bays.emplace(1, "192.168.1.163");
     }
     else{
-        bays.emplace(5, "000A470DCE5D");//port 8
-        bays.emplace(4, "000A4734C113");//port 6
-        bays.emplace(3, "000A47186BE8");//port 4
-        bays.emplace(2, "000A47044EFC");//port 2
+        bays.emplace(1, "000A47379C57");
+        bays.emplace(2, "000A47044EFC");
+        bays.emplace(3, "000A47186BE8");
+        bays.emplace(4, "000A4734C113");
+        bays.emplace(5, "000A470DCE5D");
+        bays.emplace(6, "000A4722B40D");
+        bays.emplace(7, "000A47127BB4");
+        bays.emplace(8, "000A470450A5");
+        bays.emplace(9, "000A4714F7F5");
+        bays.emplace(10, "000A47365B46");
+        bays.emplace(11, "000A4739DA62");
     }
     auto log = fulfil::utils::Logger::Instance();
     auto man = new VmbManager(bays, log);
