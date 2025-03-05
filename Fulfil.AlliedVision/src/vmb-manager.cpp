5756909af39b41d11bb9997fcfe8e40ba9a87b22
eric-fulfil <eric.hernandez@fulfil.com>
2025-03-04 22:15:39 -0800

    fix(repack): tiny sleep tuning

--- Fulfil.AlliedVision/src/vmb-manager.cpp
+++ Fulfil.AlliedVision/src/vmb-manager.cpp
@@ -96,7 +96,7 @@ void VmbManager::RunManager(){
     log_->Info("{} Cameras initialized", found);
     while (RUN){
         // Throttle queue handling slightly
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
 
         if(!service_->IsConnected())continue;
         if(!service_->HasNewRequest())continue;
