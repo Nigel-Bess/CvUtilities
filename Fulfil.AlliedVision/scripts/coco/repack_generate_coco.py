cabfc47d8426cd3817c3056aa547e9ec1360ecb9
Eric H <73857111+eric-fulfil@users.noreply.github.com>
2025-03-17 14:56:32 -0700

    remove-coco-annotator (#228)
    remove-coco-annotator (#228)

--- Fulfil.AlliedVision/scripts/coco/repack_generate_coco.py
+++ Fulfil.AlliedVision/scripts/coco/repack_generate_coco.py
@@ -4,44 +4,27 @@
 # Run from Fulfil.AlliedVision: `python3 scripts/coco/repack_generate_coco.py`
 
 from os import makedirs, listdir
-from os.path import isdir, basename, exists, abspath
+from os.path import basename, exists, join
 import shutil
 import json
-import time
-from _coco_client import CocoApiClient
 
 coco_annotator_username = "fulfil"
 coco_annotator_password = "fulfil"
-coco_client = CocoApiClient()
 
-coco_client.login(coco_annotator_username, coco_annotator_password)
-coco_client.destroy_undos() # These are just an annoyance
-
-data = "data/"
+data = "Fulfil.AlliedVision/data/"
 test = data + "test/"
 
-if not exists(data + "test"):
+if not exists(test):
     print("No COCO annotations file / test output, see README.md's Testing + Debugging section to download Repack's dataset first")
     exit(1)
 
-coco_dataset_dir = "/coco-annotator/datasets/cv-repack"
-if not exists("/coco-annotator/datasets"):
-    print("\033[93m Expected /coco-annotator sibling folder of Fulfil.ComputerVision doesn't exist, where to save coco-annotator cv-repack data?... \033[0m")
-    print("...or maybe you want to create an empty Repack Dataset in coco-annotator first? ( See ../README.md's 'Testing + Debugging' section )")
-    coco_dataset_dir = input("COCO dir: (default: " + abspath(coco_dataset_dir) + ")")
-
-# Destroy everything for this dataset in coco-annotator and start fresh
-print("\033[92m 1. Deleting and re-creating cv-repack COCO dataset \033[0m")
-coco_client.delete_dataset_by_name("cv-repack")
-print("cv-repack dataset deleted")
-#coco_client.delete_category_by_name("NotEmpty")
-#print("NotEmpty category deleted")
-ds = coco_client.create_dataset("cv-repack")
-ds_id = ds["id"]
-print("cv-repack dataset recreated")
+coco_dataset_dir = join(data, "coco")
+if not exists(coco_dataset_dir):
+    print("Created AlliedVision annotation folder")
+    makedirs(coco_dataset_dir)
 
 # Post-process by-id dataset into COCO-friendly dataset for quick eyeball review of accuracy
-print("\033[92m 2. Post-processing results to COCO format for coco-annotator \033[0m")
+print("\033[92m 2. Post-processing results to COCO format for coco annotators \033[0m")
 coco_count = 0
 coco_annotation_count = 0
 for req_id in listdir(test):
@@ -53,7 +36,7 @@ for req_id in listdir(test):
         print("\033[93m Warning: No output label found for req_id, skipping COCO-ification: " + req_id + "\033[0m")
 # Generate unified COCO file by scanning over all result files
 coco_labels = {}
-with open("scripts/coco/_coco_template.json") as f:
+with open("Fulfil.AlliedVision/scripts/coco/_coco_template.json") as f:
     coco_labels = json.load(f)
 for file in listdir(coco_dataset_dir):
     if file.endswith(".json"):
@@ -105,11 +88,4 @@ for file in listdir(coco_dataset_dir):
 with open(data + "plm.json", "w") as f:
     json.dump(coco_labels, f)
 
-time.sleep(1) # Wait for coco-annotator to catch up before uploading json
-coco_client.upload_coco_file(open(data + "plm.json", "rb"), ds_id)
-print(data + "plm.json uploaded to cv-repack COCO dataset")
-coco_client.refresh_images(ds)
-coco_client.upload_coco_file(open(data + "plm.json", "rb"), ds_id)
-
-print(coco_dataset_dir + ": " + str(coco_count) + " images, " + str(coco_annotation_count) + " non-empty annotations")
-print("\033[92m Success! View Repack PLM outputs at http://localhost:5000/#/dataset/" + str(ds_id) + "  \033[0m")
+print("Success! View Repack PLM outputs in data/coco")
