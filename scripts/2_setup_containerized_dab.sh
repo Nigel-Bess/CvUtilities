d0e2e6559dd593133ad8a914850ae442353d784d
eric-fulfil <eric.hernandez@fulfil.com>
2025-12-22 10:51:19 -0800

    fix(setup): add a dash y

--- scripts/2_setup_containerized_dab.sh
+++ scripts/2_setup_containerized_dab.sh
@@ -23,7 +23,7 @@ fi
 
 echo "Setting up GCP now..."
 sudo apt-get install -y apt-transport-https ca-certificates gnupg curl
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -y -o /usr/share/keyrings/cloud.google.gpg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 # locked on version from the TCS setup at Pioneer just in case?
 sudo apt-get install -y google-cloud-cli=524.0.0-0
