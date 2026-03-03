3eb483cddabe36d3b8f153f5c9731f201fc93a6a
Nigel-Bess <nigel.bess@fulfil.com>
2026-03-03 13:29:51 -0800

    rename to build box

--- CvBuilder.Ui/Hardcoded/Dispense.cs
+++ CvBuilder.Ui/Hardcoded/Dispense.cs
@@ -14,7 +14,7 @@ public record Dispense(string Name, SshLogin Login, Facility Facility)
             new("A2",new("fulfil@a2-dab.tan.fulfil.ai","FreshEngr") ,Facility.Tan),
             new("A3",new("fulfil@a3-dab.tan.fulfil.ai","FreshEngr") ,Facility.Tan),
         ]);
-    public static Dispense BuildBox => new("Whisman Build Box", new("fulfil@clip-tb.pioneer.fulfil.ai", "FreshEngr"), Facility.Whisman);
+    public static Dispense BuildBox => new("Build Box", new("fulfil@clip-tb.pioneer.fulfil.ai", "FreshEngr"), Facility.Whisman);
     public static Dispense P1 => new("P1", new("fulfil@p1-dab.pioneer.fulfil.ai", "FreshEngr"), Facility.Pioneer);
     public static Dispense P2 => new("P2", new("fulfil@p2-dab.pioneer.fulfil.ai", "FreshEngr"), Facility.Pioneer);
     public static IEnumerable<Dispense> GetMachines(Facility facility) => DeployableDispenses.Where(d => d.Facility == facility);
